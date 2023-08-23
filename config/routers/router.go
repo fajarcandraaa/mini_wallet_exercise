7fcfb2d84ee7dd9e7c02798c2bf457aabdd89e37
fajarcandra <fajar.candra@privyid.tech>
2023-08-23 11:37:16 +0700

    initiate redis package

--- config/routers/router.go
+++ config/routers/router.go
@@ -1,14 +1,24 @@
 package routers
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/fajarcandraaa/mini_wallet_exercise/internal/repositories"
 	"github.com/fajarcandraaa/mini_wallet_exercise/internal/service"
+	"github.com/fajarcandraaa/mini_wallet_exercise/pkg/storage/redis"
 )
 
 func (se *Serve) initializeRoutes() {
+	rds, err := redis.NewRedisConfig()
+	if err != nil {
+		fmt.Printf("Cannot connect to redis")
+		log.Fatal("This is the error:", err)
+	}
+
 	p := RouterConfigPrefix(se)            // set grouping prefix
 	r := repositories.NewRepository(se.DB) //initiate repository
-	s := service.NewService(r)             //initiate service
+	s := service.NewService(r, rds)             //initiate service
 
 	// //initiate endpoint
 	walletRouter(p, s)
--- go.mod
+++ go.mod
@@ -5,17 +5,19 @@ go 1.19
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.5.1
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
